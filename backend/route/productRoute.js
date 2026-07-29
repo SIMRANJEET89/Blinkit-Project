0db0c35ca00fca07dafad92c2fa46a863bfcb5d8
simranjeet kour <simranjeetkour8407@gmail.com>
2026-07-29 13:19:07 +0530

    add delete product route

--- backend/route/productRoute.js
+++ backend/route/productRoute.js
@@ -1,14 +1,20 @@
 import { Router } from 'express'
 import auth from '../middleware/auth.js'
-import { createProductController, getProductByCategory, getProductByCategoryAndSubCategory, getProductController, getProductDetails } from '../controllers/productController.js'
+import { createProductController, deleteProductDetails, getProductByCategory, getProductByCategoryAndSubCategory, getProductController, getProductDetails, updateProductDetails } from '../controllers/productController.js'
+import { admin } from '../middleware/Admin.js'
 
 const productRouter = Router()
 
-productRouter.post("/create", auth, createProductController)
+productRouter.post("/create", auth,admin, createProductController)
 productRouter.post("/get", getProductController)
 productRouter.post("/get-product-by-category", getProductByCategory)
 productRouter.post('/get-product-by-category-and-subcategory', getProductByCategoryAndSubCategory)
 productRouter.post('/get-product-details', getProductDetails)
+
+// update product
+productRouter.put('/update-product-details',auth,admin,updateProductDetails)
+// delete product 
+productRouter.delete('/delete-product',auth,admin,deleteProductDetails)
 
 
 export default productRouter
