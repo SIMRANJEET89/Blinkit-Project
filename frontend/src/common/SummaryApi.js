9597580f07142c104198318e98ce8c13ab90f363
simranjeet kour <simranjeetkour8407@gmail.com>
2026-07-29 13:20:10 +0530

    add delete product route in frontend

--- frontend/src/common/SummaryApi.js
+++ frontend/src/common/SummaryApi.js
@@ -98,6 +98,14 @@ const SummaryApi = {
     getProductDetails : {
         url : '/api/product/get-product-details',
         method : 'post'
+    },
+    updateProductDetails : {
+     url : '/api/product/update-product-details',
+     method : 'put'
+    },
+    deleteProduct : {
+        url : "/api/product/delete-product",
+        method : 'delete'
     }
 
 
