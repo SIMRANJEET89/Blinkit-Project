e9fd177adc72166cc2f9be80687facccfc39e930
simranjeet kour <simranjeetkour8407@gmail.com>
2026-07-30 14:01:15 +0530

    add price with discount

--- frontend/src/pages/ProductDisplayPage.jsx
+++ frontend/src/pages/ProductDisplayPage.jsx
@@ -7,9 +7,10 @@ import { FaAngleRight } from "react-icons/fa6";
 import { FaAngleLeft } from "react-icons/fa6";
 import { DisplayPriceInRupees } from "../utils/DisplayPriceInRupees";
 import Devider from "../components/Devider";
-import image1 from '../assets/minute_delivery.png'
-import image2 from '../assets/Best_Prices_Offers.png'
-import image3 from '../assets/Wide_Assortment.png'
+import image1 from "../assets/minute_delivery.png";
+import image2 from "../assets/Best_Prices_Offers.png";
+import image3 from "../assets/Wide_Assortment.png";
+import { priceWithDiscount } from "../utils/PriceWithDiscount";
 
 const ProductDisplayPage = () => {
   const params = useParams();
@@ -55,8 +56,7 @@ const ProductDisplayPage = () => {
     imageContainer.current.scrollLeft -= 100;
   };
 
-  console.log('product data', data);
-  
+  console.log("product data", data);
 
   return (
     <section className="container mx-auto p-4 grid lg:grid-cols-3">
@@ -114,57 +114,81 @@ const ProductDisplayPage = () => {
             </button>
           </div>
         </div>
-<div>
-  
-</div>
+        <div></div>
       </div>
 
       <div className="p-4 lg:pl-7 text-base lg:text-lg">
         <p className="bg-green-300 px-2 w-fit rounded-full">10 Min</p>
         <h2 className="text-lg font-semibold lg:text-3xl">{data.name}</h2>
         <p className="">Unit : {data.unit}</p>
-      <Devider/>
+        <Devider />
         <div>
           <p className="">Price :</p>
-          <div className="border border-green-500 px-4 py-2 rounded bg-green-100 w-fit">
-            <p className="font-semibold text-lg lg:text">{DisplayPriceInRupees(data.price)}</p>
+          <div className="flex items-center gap-2 lg:gap-4">
+            <div className="border border-green-500 px-4 py-2 rounded bg-green-100 w-fit">
+              <p className="font-semibold text-lg lg:text">
+                {DisplayPriceInRupees(
+                  priceWithDiscount(data.price, data.discount),
+                )}
+              </p>
             </div>
+            {
+              data.discount && (
+                <p className="line-through">{DisplayPriceInRupees(data.price)}</p>
+              )
+            }
+            {data.discount && (
+              <p className="font-bold text-green-600 lg:text-2xl ">
+                {data.discount}%{" "}
+                <span className="text-base text-neutral-400">
+                  Discount
+                </span>{" "}
+              </p>
+            )}
+          </div>
         </div>
-        {
-          data.stock === 0 ? (
-            <p className="text-lg text-red-500">Out of stock</p>
-          ) : (
-           <button className="border-green-700 bg-green-600 hover:bg-green-700 text-white px-3 py-1 m-2 rounded">Add</button>
-          )
-        }
-        
+        {data.stock === 0 ? (
+          <p className="text-lg text-red-500 my-2">Out of stock</p>
+        ) : (
+          <button className="border-green-700 bg-green-600 hover:bg-green-700 text-white px-3 py-1 m-2 rounded">
+            Add
+          </button>
+        )}
 
-       <h2 className="font-bold">Why shop from blinkit ?</h2>
-       <div>
-        <div className="flex items-center gap-4 my-4">
-          <img src={image1} alt="super-fast delivery" className="w-20 h-20"/>
-          <div className="text-sm">
-            <div className="font-semibold">SuperFast Delivery</div>
-            <p>Get your order delivered to your doorstep at the earliest from dark stores near you.</p>
-          </div>
-        </div>
-        <div className="flex items-center gap-4 my-4">
-          <img src={image2} alt="Best prices offers" className="w-20 h-20"/>
-          <div className="text-sm">
-            <div className="font-semibold">Best Prices & Offers</div>
-            <p>Best price destination with offers directly from the manufacturers.</p>
-          </div>
-        </div>
+        <h2 className="font-bold">Why shop from blinkit ?</h2>
+        <div>
           <div className="flex items-center gap-4 my-4">
-          <img src={image3} alt="Wide Assortment" className="w-20 h-20"/>
-          <div className="text-sm">
-            <div className="font-semibold">Wide Assortment</div>
-            <p>Choose from 5000+ products across food personal care, household & other categories.</p>
+            <img src={image1} alt="super-fast delivery" className="w-20 h-20" />
+            <div className="text-sm">
+              <div className="font-semibold">SuperFast Delivery</div>
+              <p>
+                Get your order delivered to your doorstep at the earliest from
+                dark stores near you.
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center gap-4 my-4">
+            <img src={image2} alt="Best prices offers" className="w-20 h-20" />
+            <div className="text-sm">
+              <div className="font-semibold">Best Prices & Offers</div>
+              <p>
+                Best price destination with offers directly from the
+                manufacturers.
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center gap-4 my-4">
+            <img src={image3} alt="Wide Assortment" className="w-20 h-20" />
+            <div className="text-sm">
+              <div className="font-semibold">Wide Assortment</div>
+              <p>
+                Choose from 5000+ products across food personal care, household
+                & other categories.
+              </p>
+            </div>
           </div>
         </div>
       </div>
-      </div>  
-      
     </section>
   );
 };
