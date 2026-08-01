f66fedc3f11eb4a5efc52e2d2767eb923abdae21
simranjeet kour <simranjeetkour8407@gmail.com>
2026-08-01 13:45:10 +0530

    fix some css classes

--- frontend/src/components/CartLoading.jsx
+++ frontend/src/components/CartLoading.jsx
@@ -1,8 +1,8 @@
 const CartLoading = () => {
   return (
-    <div className="border p-4 grid gap-3 max-w-32 lg:max-w-52 rounded animate-pulse">
-      <div className="min-h-14 lg:min-h-20 bg-blue-50 rounded"></div>
-      <div className="p-2 lg:p-3  bg-blue-50 rounded w-20"></div>
+    <div className="border py-2 lg:p-4 gap-1 grid lg:gap-3 min-w-36 lg:min-w-52 rounded bg-white animate-pulse">
+      <div className="min-h-24 lg:min-h-20 bg-blue-50 rounded"></div>
+      <div className="p-2 lg:p-3 bg-blue-50 rounded w-20"></div>
       <div className="p-2 lg:p-3 rounded bg-blue-50"></div>
       <div className="p-2 lg:p-3  bg-blue-50 rounded w-14"></div>
 
