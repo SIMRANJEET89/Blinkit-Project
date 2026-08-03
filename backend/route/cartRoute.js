6ad1547d22e191540096ea64d0228f7ba82824a1
simranjeet kour <simranjeetkour8407@gmail.com>
2026-08-03 14:52:25 +0530

    create get endpoint

---
+++ backend/route/cartRoute.js
@@ -0,0 +1,10 @@
+import {Router} from 'express'
+import auth from '../middleware/auth.js'
+import { addToCartItemController, getCartItemController } from '../controllers/cartController.js'
+
+const cartRouter = Router()
+
+cartRouter.post('/create', auth, addToCartItemController)
+cartRouter.get('/get', auth, getCartItemController)
+
+export default cartRouter
