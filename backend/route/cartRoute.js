ef021cc077650845a0ec3e7699a6f0496c25ef70
simranjeet kour <simranjeetkour8407@gmail.com>
2026-08-05 14:04:17 +0530

    add update qty url endpoint

--- backend/route/cartRoute.js
+++ backend/route/cartRoute.js
@@ -1,10 +1,12 @@
 import {Router} from 'express'
 import auth from '../middleware/auth.js'
-import { addToCartItemController, getCartItemController } from '../controllers/cartController.js'
+import { addToCartItemController, getCartItemController, updateCartItemQtyController } from '../controllers/cartController.js'
 
 const cartRouter = Router()
 
 cartRouter.post('/create', auth, addToCartItemController)
 cartRouter.get('/get', auth, getCartItemController)
+cartRouter.put('/update-qty',auth, updateCartItemQtyController)
+
 
 export default cartRouter
