0c763000f17caf6f8a8eaccdc1999a722cc46383
simranjeet kour <simranjeetkour8407@gmail.com>
2026-08-06 14:29:43 +0530

    add delete cart item api endpoint

--- backend/route/cartRoute.js
+++ backend/route/cartRoute.js
@@ -1,12 +1,13 @@
 import {Router} from 'express'
 import auth from '../middleware/auth.js'
-import { addToCartItemController, getCartItemController, updateCartItemQtyController } from '../controllers/cartController.js'
+import { addToCartItemController, deleteCartItemQtyController, getCartItemController, updateCartItemQtyController } from '../controllers/cartController.js'
 
 const cartRouter = Router()
 
 cartRouter.post('/create', auth, addToCartItemController)
 cartRouter.get('/get', auth, getCartItemController)
 cartRouter.put('/update-qty',auth, updateCartItemQtyController)
+cartRouter.delete('/delete-cart-item', auth, deleteCartItemQtyController)
 
 
 export default cartRouter
