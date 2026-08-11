059a72679977bfc64a4b553ac12a350de7d6a80b
simranjeet kour <simranjeetkour8407@gmail.com>
2026-08-11 14:49:59 +0530

    change icon

--- frontend/src/components/CartMobileLink.jsx
+++ frontend/src/components/CartMobileLink.jsx
@@ -1,29 +1,38 @@
-import React from 'react'
-import { TiShoppingCart } from "react-icons/ti"
-import { useGlobalContext } from '../Provider/GlobalProvider'
-import { DisplayPriceInRupees } from '../utils/DisplayPriceInRupees'
+import { TiShoppingCart } from "react-icons/ti";
+import { useGlobalContext } from "../Provider/GlobalProvider";
+import { DisplayPriceInRupees } from "../utils/DisplayPriceInRupees";
+import { Link } from "react-router-dom";
+import { GoTriangleRight } from "react-icons/go";
+import { useSelector } from "react-redux";
 
 const CartMobileLink = () => {
-   const {totalPrice, totalQty} = useGlobalContext()
-
+  const { totalPrice, totalQty } = useGlobalContext();
+  const cartItem = useSelector((state) => state.cartItem.cart);
 
   return (
-       <div className="bg-green-700 px-2 py-1 rounded text-white text-sm">
-          <div className='flex items-center gap-2'>
-            <div className='p-2 bg-green-500 rounded w-fit'>
-              <TiShoppingCart />
-              
+    <>
+      {cartItem[0] && (
+        <div className="sticky bottom-4 p-2">
+          <div className="bg-green-700 px-2 py-1 rounded text-white text-sm flex items-center justify-between gap-3 lg:hidden">
+            <div className="flex items-center gap-2">
+              <div className="p-2 bg-green-500 rounded w-fit">
+                <TiShoppingCart />
+              </div>
+              <div className="text-xs">
+                <p>{totalQty} items</p>
+                <p>{DisplayPriceInRupees(totalPrice)}</p>
+              </div>
             </div>
-             <div className='text-xs'>
-              <p>{totalQty} items</p>
-              <p>{DisplayPriceInRupees(totalPrice)} Price</p>
-             </div>
+
+            <Link to="/cart" className="flex items-center gap-1">
+              <span className="text-sm">View Cart</span>
+              <GoTriangleRight size={20}/>
+            </Link>
           </div>
-
-         
         </div>
-  
-  )
-}
+      )}
+    </>
+  );
+};
 
-export default CartMobileLink
+export default CartMobileLink;
