876a334679a43de197cfabd629550b2db42b4c0d
simranjeet kour <simranjeetkour8407@gmail.com>
2026-08-13 13:59:27 +0530

    create order summray and payment style

--- frontend/src/pages/CheckoutPage.jsx
+++ frontend/src/pages/CheckoutPage.jsx
@@ -1,9 +1,66 @@
-
+import { useState } from "react";
+import { useGlobalContext } from "../Provider/GlobalProvider";
+import { DisplayPriceInRupees } from "../utils/DisplayPriceInRupees";
+import AddAddress from "../components/AddAddress";
 
 const CheckoutPage = () => {
-  return (
-    <div>CheckoutPage</div>
-  )
-}
+   const { notDiscountTotalPrice, totalPrice, totalQty } = useGlobalContext();
+   const [openAddress, setOpenAddress] = useState(false)
 
-export default CheckoutPage
+
+  return (
+    <section className="bg-blue-50">
+      <div className="container mx-auto p-4 flex flex-col lg:flex-row w-full gap-5 justify-between ">
+        <div className="w-full">
+          {/* address */}
+          <h3 className="text-lg font-semibold">Choose your address</h3>
+          <div onClick={() => setOpenAddress(true)} className="h-16 bg-blue-50 border-2 border-dotted flex justify-center items-center cursor-pointer">
+            Add Address
+          </div>
+        </div>
+        <div className="w-full max-w-md bg-white py-4 px-2">
+          {/* summary */}
+          <h3 className="text-lg font-semibold">Summary</h3>
+          <div className="bg-white p-4">
+            <h3 className="font-semibold">Bill Details</h3>
+            <div className="flex gap-4 justify-between ml-1">
+              <p>Items Total</p>
+              <p className="flex items-center gap-3">
+                <span className="line-through text-neutral-400">
+                  {DisplayPriceInRupees(notDiscountTotalPrice)}
+                </span>
+                <span>{DisplayPriceInRupees(totalPrice)}</span>
+              </p>
+            </div>
+
+            <div className="flex gap-4 justify-between">
+              <p>Quantity Total</p>
+              <p className="flex items-center gap-3">{totalQty} item</p>
+            </div>
+            <div className="flex gap-4 justify-between">
+              <p>Delivery Charge</p>
+              <p className="flex items-center gap-3">Free</p>
+            </div>
+            <div className="font-semibold flex items-center justify-between gap-4">
+              <p>Grand Total</p>
+              <p>{DisplayPriceInRupees(totalPrice)}</p>
+            </div>
+          </div> 
+          <div className="w-full max-w-sm flex flex-col gap-4">
+          <button className="py-2 px-4 bg-green-600 text-white font-semibold hover:bg-green-700 rounded cursor-pointer">Online Payment</button>
+          <button className="py-2 px-4 border border-green-600 text-green-600 font-semibold hover:bg-green-600 hover:text-white rounded cursor-pointer">Cash on Delivery</button>
+          </div>
+          
+        </div>
+      </div>
+
+      {
+        openAddress && (
+          <AddAddress close={()=> setOpenAddress(false)}/>
+        )
+      }
+    </section>
+  );
+};
+
+export default CheckoutPage;
