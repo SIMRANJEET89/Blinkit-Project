9bab8a7f2ba8fc66418c006dc3555c94a6679176
simranjeet kour <simranjeetkour8407@gmail.com>
2026-08-16 14:54:04 +0530

    add update and delete route

--- backend/route/addressRoute.js
+++ backend/route/addressRoute.js
@@ -1,12 +1,13 @@
 import {Router} from 'express'
 import auth from '../middleware/auth.js'
-import { addAddressController, getAddresController } from '../controllers/AddressController.js'
+import { addAddressController, deleteAddressController, getAddresController, updateAddressController } from '../controllers/AddressController.js'
 
 
 const addressRouter = Router()
 
 addressRouter.post('/create', auth, addAddressController)
 addressRouter.get('/get',auth, getAddresController)
-
+addressRouter.put('/update',auth, updateAddressController)
+addressRouter.delete('/delete',auth,deleteAddressController)
 
 export default addressRouter
