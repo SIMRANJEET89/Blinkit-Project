4e51f4c557731e8a736a79cd8e73705c4a2bd690
simranjeet kour <simranjeetkour8407@gmail.com>
2026-08-16 14:57:44 +0530

    update addressline

--- frontend/src/pages/CheckoutPage.jsx
+++ frontend/src/pages/CheckoutPage.jsx
@@ -5,13 +5,13 @@ import AddAddress from "../components/AddAddress";
 import { useSelector } from "react-redux";
 
 const CheckoutPage = () => {
-   const { notDiscountTotalPrice, totalPrice, totalQty } = useGlobalContext();
-   const [openAddress, setOpenAddress] = useState(false)
-   const addressList = useSelector(state => state.addresses.addressList)
+  const { notDiscountTotalPrice, totalPrice, totalQty } = useGlobalContext();
+  const [openAddress, setOpenAddress] = useState(false);
+  const addressList = useSelector((state) => state.addresses.addressList);
+  const [selectedAddress, setSelectedAddress] = useState(0);
 
+  console.log("selected add", addressList[selectedAddress]);
 
-   console.log('addressList', addressList);
-   
   return (
     <section className="bg-blue-50">
       <div className="container mx-auto p-4 flex flex-col lg:flex-row w-full gap-5 justify-between">
@@ -19,24 +19,39 @@ const CheckoutPage = () => {
           {/* address */}
           <h3 className="text-lg font-semibold">Choose your address</h3>
           <div className="bg-white p-3 grid gap-4">
-            {
-              addressList.map((address,index) =>{
-                return(
-                  <div className="border rounded p-2">
-                    <p>{address.address_line}</p>
-                    <p>{address.city}</p>
-                    <p>{address.state}</p>
-                    <p>{address.country} - {address.pincode}</p>
-                    <p>{address.mobile}</p>
+            {addressList.map((address, index) => {
+              return (
+                <label htmlFor={"address"+index}>
+                  <div className="border rounded p-3 flex gap-3 hover:bg-blue-50">
+                    <div className="">
+                      <input
+                        id={"address"+index}
+                        type="radio"
+                        name="address"
+                        value={index}
+                        onChange={(e) => setSelectedAddress(e.target.value)}
+                      />
+                    </div>
+                    <div>
+                      <p>{address.address_line}</p>
+                      <p>{address.city}</p>
+                      <p>{address.state}</p>
+                      <p>
+                        {address.country} - {address.pincode}
+                      </p>
+                      <p>{address.mobile}</p>
+                    </div>
                   </div>
-                )
-              })
-            }
-           <div onClick={() => setOpenAddress(true)} className="h-16 bg-blue-50 border-2 border-dotted flex justify-center items-center cursor-pointer">
-            Add Address
+                </label>
+              );
+            })}
+            <div
+              onClick={() => setOpenAddress(true)}
+              className="h-16 bg-blue-50 border-2 border-dotted flex justify-center items-center cursor-pointer"
+            >
+              Add Address
+            </div>
           </div>
-          </div>
-          
         </div>
         <div className="w-full max-w-md bg-white py-4 px-2">
           {/* summary */}
@@ -65,20 +80,19 @@ const CheckoutPage = () => {
               <p>Grand Total</p>
               <p>{DisplayPriceInRupees(totalPrice)}</p>
             </div>
-          </div> 
-          <div className="w-full max-w-sm flex flex-col gap-4">
-          <button className="py-2 px-4 bg-green-600 text-white font-semibold hover:bg-green-700 rounded cursor-pointer">Online Payment</button>
-          <button className="py-2 px-4 border border-green-600 text-green-600 font-semibold hover:bg-green-600 hover:text-white rounded cursor-pointer">Cash on Delivery</button>
           </div>
-          
+          <div className="w-full max-w-sm flex flex-col gap-4">
+            <button className="py-2 px-4 bg-green-600 text-white font-semibold hover:bg-green-700 rounded cursor-pointer">
+              Online Payment
+            </button>
+            <button className="py-2 px-4 border border-green-600 text-green-600 font-semibold hover:bg-green-600 hover:text-white rounded cursor-pointer">
+              Cash on Delivery
+            </button>
+          </div>
         </div>
       </div>
 
-      {
-        openAddress && (
-          <AddAddress close={()=> setOpenAddress(false)}/>
-        )
-      }
+      {openAddress && <AddAddress close={() => setOpenAddress(false)} />}
     </section>
   );
 };
